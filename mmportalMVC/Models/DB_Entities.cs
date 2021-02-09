f74f6edf86c7ea44e67787d64d76de461e20a060
Martin Olsen <mwolsen123@gmail.com>
2021-02-10 00:42:51 +0100

    Opsætning af model til DB og login/register funktionalitet

---
+++ mmportalMVC/Models/DB_Entities.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace mmportalMVC.Models
+{
+    public class DB_Entities : DbContext
+    {
+        public DB_Entities() : base("mellemmanden") { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
